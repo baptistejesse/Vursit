16362e5328a11f753eed221e13f9e3f0e4d84fc1
baptistejesse <baptistejesse@yahoo.com>
2012-06-26 12:26:24 -0400

    changed css

--- spec/video_spec.rb
+++ spec/video_spec.rb
@@ -1,21 +1,25 @@
 require 'spec_helper'
 
 describe Video do
+  
+  before(:each) do
+  @video = Video.new
+  end
  it 'should have a title with zambie' do
- video = Video.new
- video.title = "zambie"
- video.title.should == "zambie"
+
+ @video.title = "zambie"
+ @video.title.should == "zambie"
  end
  
  it 'should not be valid if video not http' do
- video = Video.new
- video.viddy = "bombom"
- video.title = "zambie"
- video.should_not be_valid
+ 
+ @video.viddy = "bombom"
+ @video.title = "zambie"
+ @video.should_not be_valid
  end
  
  it 'should be valid if video has http' do
- @video = Video.new
+
  @video.title = "hello"
  @video.viddy = "http://www.google.com"
  @video.should be_valid
@@ -26,12 +30,12 @@ describe Video do
  
  
  it "should have 1 vidvid" do
-   @video = Video.new
+  
    @video.title = "hello"
    @video.viddy = "http://www.google.com"
    @video.save
- @vidvid = @video.vidvids.create(video_id: @video.id, viddy1: "ello", title1: "http://www.google.com" )  
- @video.vidvids.count.should == 1
+ @vidvid = @video.create_vidvid(video_id: @video.id, viddy1: "ello", title1: "http://www.google.com" )  
+ @video.vidvid.should be_present
 end   
    
  
