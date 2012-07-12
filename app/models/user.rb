b7f49697da9d14171d89e4ae6c9994b00d2f7190
baptistejesse <baptistejesse@yahoo.com>
2012-07-12 10:54:46 -0400

    again

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
       user
   else # Create a user with a stub password.
       User.create!(:email => data.email, :password => Devise.friendly_token[0,20],
-                   :name => data.name , :gender => data.gender, :pic => pic.image)
+                   :name => data.name , :gender => data.gender, :pic => data.username)
   end 
 end
 
